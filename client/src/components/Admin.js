e0a7cd1f91471eff256aadb298a0e427981cff02
Ethan <e.cowey1@newcastle.ac.uk>
2022-04-13 09:52:11 +0100

    added presale and user input to forms

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -6,7 +6,7 @@ class Admin extends Component {
 
     addEvent = async () => {
         const { ticketContract, accounts} = this.props.parentState;
-        console.log(ticketContract);
+        console.log(document.getElementById("name").value + document.getElementById("seats").value);
         console.log(accounts);
         //const hex = '0x'+Eth.Buffer
         // Stores a given value, 5 by default.
@@ -21,8 +21,8 @@ class Admin extends Component {
             return res
             //this.setState({ storageValue: res });
         });
-        await ticketContract.methods.addEvent("eventName", 5, false).send({from: accounts[0]});
-        await ticketContract.methods.eventDetails(1).call(function (err, res) {
+        await ticketContract.methods.addEvent(document.getElementById("name").value, document.getElementById("seats").value, document.getElementById("presale").value).send({from: accounts[0]});
+        await ticketContract.methods.eventDetails(6).call(function (err, res) {
             if (err) {
                 console.log("An error occured", err)
                 return
@@ -42,7 +42,14 @@ class Admin extends Component {
                     If your contracts compiled and migrated successfully, below will show
                     a stored value of 5 (by default).
                 </p>
-                <button onClick={this.addEvent}>Buy a new ticket from the vendor for 1 eth</button>
+                <h2>Add a new event</h2>
+                <label>Event Name</label>
+                <input type="string" name="event name" id="name"/>
+                <label>Seats</label>
+                <input type="number" name="seats available" id="seats"/>
+                <label>On presale</label>
+                <input type="bool" name="On presale" id="presale"/>
+                <button onClick={this.addEvent}>Add new event</button>
             </div>
         );
     }
